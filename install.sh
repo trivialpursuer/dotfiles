5b374149a71f0a194d391d758ed79c5c2004ec27
Trevor Phillips <trevor.phillips@bird.co>
2021-04-16 11:11:26 -0700

    i3 Initial Commit
    First addition of i3 Customization

--- install.sh
+++ install.sh
@@ -11,3 +11,10 @@ ln -sf "$HOME/projects/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 #############
 # rm -rf "$HOME/.config/X11"
 ln -sf "$HOME/projects/dotfiles/X11/.Xresources" "$HOME/.Xresources"
+
+######
+# i3 #
+######
+
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/projects/dotfiles/i3" "$HOME/.config"
